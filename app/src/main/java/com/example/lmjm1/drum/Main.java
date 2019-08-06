43befb08bcafdade63960a7058bc37e1953dc340
jiminlim <39897338+jiminlim@users.noreply.github.com>
2019-08-06 13:05:56 +0900

    jimin8
    임시저장

--- app/src/main/java/com/example/lmjm1/drum/Main.java
+++ app/src/main/java/com/example/lmjm1/drum/Main.java
@@ -4,11 +4,11 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Play extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+        setContentView(R.layout.activity_main);
     }
 }
